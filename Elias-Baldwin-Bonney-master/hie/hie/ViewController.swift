0a79b7ba6a730450580f9f6228862516f7d88ebb
3lias30 <elias@my.prideprepschool.org>
2017-04-27 13:08:19 -0700

    Update 2 - HW1
    - Fixed Fullchange Button
    - Still have run bug

---
+++ Elias-Baldwin-Bonney-master/hie/hie/ViewController.swift
@@ -0,0 +1,58 @@
+//
+//  ViewController.swift
+//  hie
+//
+//  Created by Student on 4/24/17.
+//  Copyright © 2017 Student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+
+    @IBOutlet weak var helloWorld: UILabel!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet weak var colorChange: UIButton!
+    @IBOutlet weak var Fullchange2: UIButton!
+    @IBOutlet weak var Textcolor: UIButton!
+    var index = 0;
+    
+    let list = [UIColor.blue, UIColor.green, UIColor.magenta]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        colorChange.isHidden = true;
+        helloWorld.isHidden = true;
+        Textcolor.isHidden = true;
+        Fullchange2.isHidden = true;
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func PlayButton(_ sender: Any) {
+        helloWorld.isHidden = false;
+        playButton.isHidden = true;
+        colorChange.isHidden = false;
+    }
+    @IBAction func Colorchange(_ sender: Any) {
+        helloWorld.text = "I CAN CHANGE COLOR"
+        Fullchange2.isHidden = false;
+        colorChange.isHidden = true;    
+    }
+    @IBAction func fullChange2(_ sender: Any) {
+        helloWorld.text = "COLOR CHANGER"
+        Fullchange2.isHidden = true;
+        Textcolor.isHidden = false;
+    }
+    @IBAction func textColor(_ sender: Any) {
+        helloWorld.backgroundColor = list[index]
+        index = (index + 1) % list.count;
+    }
+
+}
+
