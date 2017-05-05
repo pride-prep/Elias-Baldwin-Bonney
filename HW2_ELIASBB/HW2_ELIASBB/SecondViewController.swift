39208ed206513b9a3dbc5dd78491a8199970c1c7
3lias30 <elias@my.prideprepschool.org>
2017-05-05 11:10:32 -0700

    HW2 = Update 2
    -Main buttons done
    -Edit on color photo

---
+++ HW2_ELIASBB/HW2_ELIASBB/SecondViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.swift
+//  HW2_ELIASBB
+//
+//  Created by Student on 5/5/17.
+//  Copyright © 2017 Student. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: NSObject {
+
+}
--- HW2_ELIASBB/HW2_ELIASBB/ViewController.swift
+++ HW2_ELIASBB/HW2_ELIASBB/ViewController.swift
@@ -9,9 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var mainImage: UIImageView!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var HowmanyPlayers: UILabel!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    @IBOutlet weak var button6: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        HowmanyPlayers.isHidden = true
+        button2.isHidden = true
+        button3.isHidden = true
+        button4.isHidden = true
+        button5.isHidden = true
+        button6.isHidden = true
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -19,7 +33,16 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    @IBAction func startButton(_ sender: Any) {
+        mainImage.isHidden = true
+        startButton.isHidden = true
+        HowmanyPlayers.isHidden = false
+        button2.isHidden = false
+        button3.isHidden = false
+        button4.isHidden = false
+        button5.isHidden = false
+        button6.isHidden = false
+    }
 
 }
 
